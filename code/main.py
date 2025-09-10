75a6eed182bf7e93462cfdcdccdbab2976da966f
Trodyy <mahdi.karimi.beiranvand2003@gmail.com>
2025-09-10 17:58:37 +0330

    FEAT : Connect to minio and Create a new buccket.

--- code/main.py
+++ code/main.py
@@ -1,10 +1,17 @@
 from minio import Minio
+from minio.error import S3Error
 
 client = Minio(
-    endpoint="minio:9000",
-    access_key="minio",     
-    secret_key="minio123" ,
-    secure=False                
+    "minio:9000",  
+    access_key="minio", 
+    secret_key="minio123", 
+    secure=False
 )
 
-print("Total buckets:", len(client.list_buckets()))
+b_name = "python-demo-bucket"
+
+if not client.bucket_exists(b_name):
+    client.make_bucket(b_name)
+    print("Created bucket:", b_name)
+else:
+    print(b_name, "already exists.")

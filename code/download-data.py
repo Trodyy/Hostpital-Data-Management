0114a2f5cd5a7a77899cb6c53e504cfee1dcec8d
Trodyy <mahdi.karimi.beiranvand2003@gmail.com>
2025-09-11 14:18:19 +0330

    FEAT : Add data to project from minio.

--- code/download-data.py
+++ code/download-data.py
@@ -1,65 +1,24 @@
 from minio import Minio
+from minio.error import S3Error
 
-### connect to minio server ####
 client = Minio(
-    "minio:9000",  
-    access_key="minio", 
-    secret_key="minio123", 
+    "minio:9000",
+    access_key="minio",
+    secret_key="minio123",
     secure=False
 )
-### Creating Buckets ####
-b_name = "python-demo-bucket"
-client.make_bucket(b_name)
-if client.bucket_exists(b_name) :
-    print(f"{b_name} already exists.")
-else :
-    print(f"Created bucket {b_name}.")
+
+bucket_name = "hospital-data"
+
+if not client.bucket_exists(bucket_name):
+    client.make_bucket(bucket_name)
+    print(f"Bucket '{bucket_name}' created.")
+else:
+    print(f"Bucket '{bucket_name}' already exists.")
 
 
-### Listing Buckets ####
-buckets = client.list_buckets()
-for bucket in buckets:
-    print(f"Bucket_name: {bucket.name}, Created on: {bucket.creation_date}")
-
-
-### Uploading Objects ####
-b_name = "python-demo-bucket"
-o_name = "test.txt"
-f_name = "a.txt"
-c_type = "text/plain" 
-result = client.fput_object(b_name, o_name, f_name, c_type)
-print(
-    "Created {0} with etag {1}, version-id {2}".format(
-        result.object_name, result.etag, result.version_id
-    )
-)
-
-### download file from minio ####
-b_name = "python-demo-bucket"
-o_name = "test.txt"
-f_name = "shirin.txt"
-
-result = client.fget_object(b_name , o_name , f_name)
-print(
-    "download {0} with etag {1}, version-id {2}".format(
-        result.object_name, result.etag, result.version_id
-    )
-)
-
-
-
-### Listing objects in a bucket ####
-b_name = "python-demo-bucket"
-objects = client.list_objects(b_name , include_version=True , recursive=True)
-for obj in objects:
-    print(f"Object: {obj.object_name}, Size: {obj.size}, Last Modified: {obj.last_modified}")
-
-
-### Deleting an object ####
-b_name = "python-demo-bucket"
-o_name = "test.txt"
-client.remove_object(b_name , o_name)
-print(f"Deleted object {o_name} from bucket {b_name}.")
-
-
-        
+appointment_result = client.fget_object("hospital-data" , "appointments.csv" , "appointments.csv")
+billing_result = client.fget_object("hospital-data" , "billing.csv" , "billing.csv")
+doctor_result = client.fget_object("hospital-data" , "doctors.csv" , "doctors.csv")
+patient_result = client.fget_object("hospital-data" , "patients.csv" , "patients.csv")
+treatment_result = client.fget_object("hospital-data" , "treatments.csv" , "treatmentss.csv")
